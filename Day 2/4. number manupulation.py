6936353188b7b49f995ae0caf5e675f98bc5f7cc
anshuman <anshumanhota12@gmail.com>
2025-06-11 00:04:25 +0530

    adding files

---
+++ Day 2/4. number manupulation.py
@@ -0,0 +1,45 @@
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+#
+# print(int(bmi))
+#
+# print(round(bmi))
+#
+# print(round(bmi, 2))
+
+# Assignment Operators
+
+# score = 0
+# # score = score + 1
+# score += 1
+# print(score)
+#
+# score -= 1
+# print(score)
+#
+# score *= 1
+# print(score)
+#
+# score /= 1
+# print(score)
+
+# f-Strings
+
+age = 12
+
+print(f"I am {age} years old")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
